2a800664cb8b38e72a92f3910687fef72bfa16fc
iwebwork <iwebwork.adm@gmail.com>
2019-08-21 12:13:42 -0300

    ajustes no link da logo

--- views/templateOne.php
+++ views/templateOne.php
@@ -4,7 +4,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-        <link rel="shortcut icon" type="image/x-icon" href="https://drive.google.com/open?id=1uyIALvwYLxirgbjsEDcQ9eKVgcRxrJ2P"/>
+        <link rel="shortcut icon" type="image/x-icon" href="<?php echo BASE_URL;?>assets/images/logo.png"/>
         <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL;?>assets/css/bootstrap.min.css"/>
         <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL;?>assets/css/mdb.min.css"/>
         <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL;?>assets/css/mdb.css"/>
@@ -22,7 +22,7 @@
     <body onload="date()">
         <nav class="navbar navbar-expand-lg navbar-light style-nave font-title fudoImg">
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-               <img class="img-logo" src="https://drive.google.com/open?id=1uyIALvwYLxirgbjsEDcQ9eKVgcRxrJ2P" >
+               <img class="img-logo" src="<?php echo BASE_URL;?>assets/images/logo.png" >
             </button>
 
             <div class="collapse navbar-collapse d-flex justify-content-center" id="navbarTogglerDemo03">
