27374da6d8b70e28336eccf08f554e4b0d5d1bcd
iwebwork <iwebwork.adm@gmail.com>
2020-07-15 16:40:45 -0300

    Ajuste no conteudo da tela home

--- Views/home.php
+++ Views/home.php
@@ -4,13 +4,14 @@
             <h4 class="font-weight-bold text-center" ">Web And Android Developer</h4>
         </div>
     </div>
+    <br/>
     <div class="row d-flex justify-content-center">
         <div class="col-sm-3 d-flex flex-column">
             <div class="p-2 d-flex">
                 <h6 class="">
-                    My name is Igor Siqueira da Silva. I'm 23 years old.<br><br>
+                    My name is Igor Siqueira da Silva. I'm 24 years old.<br><br>
                     I currently work with Web Development and Android Development.<br><br>
-                    I'm currently studying Information Systems at UNA-Contagem, and I am in the fifth semester.
+                    I'm currently studying Information Systems at UNA-Contagem, and I am in the sixth semester.
                 </h6>
             </div>
             
